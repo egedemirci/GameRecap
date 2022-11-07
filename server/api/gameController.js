4ff132ae20403e565f516844a1fa2def0b8591c1
DenizGezgin <denizgzgn@gmail.com>
2022-11-07 23:24:02 +0300

    game api

--- server/api/gameController.js
+++ server/api/gameController.js
@@ -12,15 +12,108 @@ export default class gameController{
             }
           })
         } catch (error) {
-          console.log(`Error when getting all games ${error.detail}`)
+          console.log(`Error when getting all games ${error}`)
           res.status(400).json({error:error, data:{games:[]}})
         }   
     }
 
+    static async getGameById(req, res, next){
+        try {
+          const results = await db.query('SELECT * FROM game_recap.Games WHERE game_id = $1', [req.params.id])
+            
+          if(results.rows.length == 0)
+            {
+              throw {
+                detail: "Game not found.",
+                code: 1,
+                error: new Error()
+              };
+            }
 
 
+          res.status(200).json({
+            lenght: results.rows.length,
+            data: results.rows
+          })
+        } catch (error) {
+            if(err.code == 1)
+            {
+              res.status(404).json({detail:err.detail, data:[]})
+              return
+            }
+            console.log(`Error when getting game by id ${error}`)
+            res.status(400).json({error:error, data:[]})
+        }   
+    }
 
 
+    static async createGame(req, res, next){
+        try {         
+            const newGame = await db.query("INSERT INTO game_recap.Games (game_name,release_date) values ($1, $2) returning *"
+            , [req.body.game_name, "2001-12-30"])
+            console.log(newGame)
+            res.status(200).json({
+              data: newGame.rows[0]
+            })
+    
+          } catch (error) {
+            console.log(`Error when creating game ${error}`)
+            res.status(400).json({error:error, data:[]})
+          }  
+    }
 
+    static async updateGame(req, res, next){
+        try {         
+            const updatedGame = await db.query('UPDATE game_recap.Games SET name = $2 ,release_date = $3 WHERE game_id = $1 returning *'
+            , [req.params.id, req.body.name, req.body.release_date])
+
+            if(results.rows.length == 0)
+            {
+            throw {
+                detail: "Game not found.",
+                code: 1,
+                error: new Error()
+              };
+            }
+
+
+            res.status(200).json({
+              data: updatedGame.rows[0]
+            })
+    
+          } catch (error) {
+            console.log(`Error when updating game ${error}`)
+            if(err.code == 1)
+            {
+              res.status(404).json({detail:err.detail, data:[]})
+              return
+            }
+            res.status(400).json({error:error, data:[]})
+          }  
+    }
+
+    static async deleteById(req, res, next){
+        try {
+          const results = await db.query("DELETE FROM game_recap.Games WHERE game_id = $1 returning *", [req.params.id])
+          if(results.rows.length == 0)
+            {
+              throw {
+                detail: "Game not found.",
+                code: 1,
+                error: new Error()
+              };
+            }
+    
+          res.status(200).json({data: results.rows[0]})
+        } catch (err) {
+          console.log(`Failed to delete user ${err}.`)
+            if(err.code == 1)
+            {
+              res.status(404).json({detail:err.detail, data:[]})
+              return
+            }
+            res.status(400).json({detail:err, data:[]})
+        }   
+      }
 
 }
--- server/api/gameRecap.route.js
+++ server/api/gameRecap.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import gameController from "./gameController.js";
 
 //Api calls are done through uri and router routes uri correct functions
 //get request send through uri > router makes correct controllerCall > controller makes DBOcall > DBO returns results from db to controller > controller adds json to response of the request
@@ -8,9 +9,18 @@ router.use(express.json())
 
 //tell a route to look for a GET request on the root "/" URL and return some JSON:
 router.route("/test").get((req, res) => {
-    res.json({ info: 'You request to GameRecap has been received!' })
-  })
+  res.json({ info: 'You request to GameRecap has been received!' })
+})
 
+router.route("/games")
+  .get(gameController.getAllGames)
+  .post(gameController.createGame)
+
+
+router.route("/games/:id")
+  .get(gameController.getGameById)
+  .patch(gameController.updateGame)
+  .delete(gameController.deleteById)
 
 
 
