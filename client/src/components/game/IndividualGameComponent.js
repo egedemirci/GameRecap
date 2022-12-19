97f6ee90089ae6ba86945ba61ca0299adda62c5b
Efe Tuzun <efetuzun@sabanciuniv.edu>
2022-12-20 01:16:42 +0300

    Game Add
    Co-Authored-By: Ahmet Furkan Un <69799276+ahmtfurknun@users.noreply.github.com>

--- client/src/components/game/IndividualGameComponent.js
+++ client/src/components/game/IndividualGameComponent.js
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Avatar, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import SmallCard from "../SmallCard";
 import GameFinder from "../../apis/GameFinder";
 import ResponsiveAppBar from "../appbarGame";
@@ -16,54 +23,57 @@ export default function IndividualGameComponent() {
   const theme = useTheme();
 
   const refreshPage = () => {
-    console.log("Fresh")
-    window.location.reload()
-  }
-  
-  function renderReview(text, username)
-  {
-    return(
+    console.log("Fresh");
+    window.location.reload();
+  };
+
+  function renderReview(text, username) {
+    return (
       <Card
-            sx={{ height: 250, width: 500 }}
-            style={{ backgroundColor: theme.palette.secondary.main }}
+        sx={{ height: 250, width: 500 }}
+        style={{ backgroundColor: theme.palette.secondary.main }}
+      >
+        <CardContent>
+          <Grid
+            container
+            spacing={2}
+            direction="column"
+            justifyContent="center"
+            alignItems="flex-start"
           >
-            <CardContent>
-              <Grid
-                container
-                spacing={2}
-                direction="column"
-                justifyContent="center"
-                alignItems="flex-start"
-              >
-                <Grid item>
-                  <Typography style={{ fontWeight: "bold", fontSize: 15 }}>{username} </Typography>
-                </Grid>
-      
-                <Grid item>
-                  <Typography sx={{ height: 150, width: 400}}> {text} </Typography>
-                </Grid>
-                </Grid>
-            </CardContent>
-          </Card>
-    )
+            <Grid item>
+              <Typography style={{ fontWeight: "bold", fontSize: 15 }}>
+                {username}{" "}
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography sx={{ height: 150, width: 400 }}> {text} </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    );
   }
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await GameFinder.get(`/games/${id}`);
       setGame(result.data.data);
-      const reviewRes = await GameFinder.put(`/rate/?gid=${id}`)
-      setReviews(reviewRes.data.data)
+      console.log(game);
+      const reviewRes = await GameFinder.put(`/rate/?gid=${id}`);
+      setReviews(reviewRes.data.data);
     };
     fetchData();
   }, [id]);
+
   return game !== null ? (
     <>
       <ResponsiveAppBar />
       <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-        <Grid item mr={2} mt = {2} xs={3} md={0} lg={12}>
+        <Grid item mr={2} mt={2} xs={3} md={0} lg={12}>
           <center>
-          <center>
+            <center>
               <Avatar
                 src="https://i.hizliresim.com/kti4lvy.png"
                 sx={{ width: 150, height: 150 }}
@@ -83,15 +93,15 @@ export default function IndividualGameComponent() {
           <SmallCard title="Release Date" subtitle={game.release_date} />
         </Grid>
         <Grid item xs={12} mt={0} sm={6} md={4} lg={3}>
-          <SmallCard title="Languages" subtitle={game.lang_name,"TR"} />
+          <SmallCard title="Languages" subtitle={game.lang_name} />
         </Grid>
         <Grid item xs={12} mt={0} sm={6} md={4} lg={3}>
-          <SmallCard title="On Stores" subtitle={game.store_name,"Steam"} />
+          <SmallCard title="On Stores" subtitle={game.store_name} />
         </Grid>
         <Grid item xs={12} mt={0} sm={6} md={4} lg={3}>
           <SmallCard
             title="Offered In Services"
-            subtitle={game.service_names, "XBOX Game Pass"}
+            subtitle={(game.service_names, "XBOX Game Pass")}
           />
         </Grid>
         <Grid
@@ -100,16 +110,25 @@ export default function IndividualGameComponent() {
           sx={{ display: { sm: "none", md: "block", lg: "none" } }}
         />
         <Grid item ml={2} mt={0} xs={12} sm={6} md={4} lg={2.5}>
-          <SmallCard title="DLCs" subtitle={game.dlc_name, "Tomorrow"} />
+          <SmallCard title="DLCs" subtitle={(game.dlc_name, "Tomorrow")} />
         </Grid>
         <Grid item xs={12} mt={0} sm={6} md={4} lg={3}>
-          <SmallCard title="By Development Studios" subtitle={game.ds_name,"Rockstar"} />
+          <SmallCard
+            title="By Development Studios"
+            subtitle={(game.ds_name, "Rockstar")}
+          />
         </Grid>
         <Grid item xs={12} mt={0} sm={6} md={4} lg={3}>
-          <SmallCard title="Categories" subtitle={game.category_name,"Sports"} />
+          <SmallCard
+            title="Categories"
+            subtitle={(game.category_name, "Sports")}
+          />
         </Grid>
         <Grid item xs={12} mt={0} sm={6} md={4} lg={3}>
-          <SmallCard title="On Platforms" subtitle={game.platform_name, "Playstation"} />
+          <SmallCard
+            title="On Platforms"
+            subtitle={(game.platform_name, "Playstation")}
+          />
         </Grid>
         <Grid
           item
@@ -118,25 +137,46 @@ export default function IndividualGameComponent() {
         />
         <Grid item xs={1} />
         <Grid item xs={10} align="center">
-          <SmallCard title="Synopsis" subtitle={game.synopsis, "In God of War, players control Kratos, a Spartan warrior who is sent by the Greek gods to kill Ares, the god of war. As the story progresses, Kratos is revealed to be Ares’ former servant, who had been tricked into killing his own family and is haunted by terrible nightmares. Armed with the Blades of Chaos, a weapon made out of two daggers attached to chains, Kratos rumbles through ancient Athens and other locations on a murderous quest to terminate the rogue god. Action in God of War is viewed from the third person, and advanced movements such as running, jumping, climbing, and swimming are similar to those in the Tomb Raider series, another adventure-game series with strong platform-game characteristics. Some of Kratos’s foes can be killed only by combinations of magic and physical attacks, making combat more reliant on skill. "} />
+          <SmallCard
+            title="Synopsis"
+            subtitle={
+              (game.synopsis,
+              "In God of War, players control Kratos, a Spartan warrior who is sent by the Greek gods to kill Ares, the god of war. As the story progresses, Kratos is revealed to be Ares’ former servant, who had been tricked into killing his own family and is haunted by terrible nightmares. Armed with the Blades of Chaos, a weapon made out of two daggers attached to chains, Kratos rumbles through ancient Athens and other locations on a murderous quest to terminate the rogue god. Action in God of War is viewed from the third person, and advanced movements such as running, jumping, climbing, and swimming are similar to those in the Tomb Raider series, another adventure-game series with strong platform-game characteristics. Some of Kratos’s foes can be killed only by combinations of magic and physical attacks, making combat more reliant on skill. ")
+            }
+          />
         </Grid>
         <Grid item xs={5} align="center">
-          <Grid container spacing={3}
-          direction="column"
-          justifyContent="center"
-          alignItems="center" >
-            <Grid item><RateCard user_id={user.user_id} game_id={id} refreshPage={refreshPage}/> </Grid >
-            <Grid item><Typography style={{ fontWeight: "bold", fontSize: 30 }}>All Reviews</Typography></Grid >
-            <Grid item><Stack>
-            <div>
-      {reviews.map(review => (
-        <div key={review.user_id}>
-          {renderReview(review.text, review.username)}
-        </div>
-      ))}
-    </div>
-              </Stack></Grid>
-          </Grid >
+          <Grid
+            container
+            spacing={3}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <RateCard
+                user_id={user.user_id}
+                game_id={id}
+                refreshPage={refreshPage}
+              />{" "}
+            </Grid>
+            <Grid item>
+              <Typography style={{ fontWeight: "bold", fontSize: 30 }}>
+                All Reviews
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Stack>
+                <div>
+                  {reviews.map((review) => (
+                    <div key={review.user_id}>
+                      {renderReview(review.text, review.username)}
+                    </div>
+                  ))}
+                </div>
+              </Stack>
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item xs={1} />
         <Grid
--- server/api/gameController.js
+++ server/api/gameController.js
@@ -18,7 +18,10 @@ export default class gameController {
 
   static async getAllPlaylists(req, res, next) {
     try {
-      const results = await db.query("SELECT * FROM game_recap.playlists WHERE user_id = $1" , [req.params.id]);
+      const results = await db.query(
+        "SELECT * FROM game_recap.playlists WHERE user_id = $1",
+        [req.params.id]
+      );
       res.status(200).json({
         lenght: results.rows.length,
         data: {
@@ -33,7 +36,9 @@ export default class gameController {
 
   static async getPlaylistsGenres(req, res, next) {
     try {
-      const results = await db.query("with temp as (select gp.playlist_id, playlist_name, category_name, count(*) as count from game_recap.gamesplaylists gp left join game_recap.gamescategories gc using(game_id) left join game_recap.categories c on gc.c_id = c.c_id left join game_recap.playlists p on gp.playlist_id = p.playlist_id group by gp.playlist_id, playlist_name, category_name), max as (select playlist_id, max(count) as count from temp group by 1) select playlist_id, playlist_name, string_agg(category_name, ', ') from temp right join max using (playlist_id, count) group by 1,2");
+      const results = await db.query(
+        "with temp as (select gp.playlist_id, playlist_name, category_name, count(*) as count from game_recap.gamesplaylists gp left join game_recap.gamescategories gc using(game_id) left join game_recap.categories c on gc.c_id = c.c_id left join game_recap.playlists p on gp.playlist_id = p.playlist_id group by gp.playlist_id, playlist_name, category_name), max as (select playlist_id, max(count) as count from temp group by 1) select playlist_id, playlist_name, string_agg(category_name, ', ') from temp right join max using (playlist_id, count) group by 1,2"
+      );
       console.log(results);
       res.status(200).json({
         lenght: results.rows.length,
@@ -47,11 +52,12 @@ export default class gameController {
     }
   }
 
-
-
   static async getPlaylistById(req, res, next) {
     try {
-      const results = await db.query("SELECT g.game_name, g.game_id FROM game_recap.games g, game_recap.gamesplaylists p WHERE g.game_id = p.game_id AND p.playlist_id = $1;" , [req.params.id]);
+      const results = await db.query(
+        "SELECT g.game_name, g.game_id FROM game_recap.games g, game_recap.gamesplaylists p WHERE g.game_id = p.game_id AND p.playlist_id = $1;",
+        [req.params.id]
+      );
       res.status(200).json({
         lenght: results.rows.length,
         data: {
@@ -64,34 +70,33 @@ export default class gameController {
     }
   }
 
-
-  static async getUserById(req, res, next){
+  static async getUserById(req, res, next) {
     console.log(`here`);
     try {
-
-      const result = await db.query('SELECT * FROM game_recap.users WHERE user_id = $1', [req.params.id])
+      const result = await db.query(
+        "SELECT * FROM game_recap.users WHERE user_id = $1",
+        [req.params.id]
+      );
       console.log("aaa");
-      if(result.rows.length == 0)
-      {
+      if (result.rows.length == 0) {
         throw {
           detail: "User not found.",
           code: 1,
-          error: new Error()
+          error: new Error(),
         };
       }
 
       res.status(200).json({
-      data: result.rows[0]
-      })
+        data: result.rows[0],
+      });
     } catch (err) {
-      console.log(`Error when getting one user ${err}`)
-      if(err.code == 1)
-      {
-        res.status(404).json({detail:err.detail, data:[]})
-        return
+      console.log(`Error when getting one user ${err}`);
+      if (err.code == 1) {
+        res.status(404).json({ detail: err.detail, data: [] });
+        return;
       }
-      res.status(400).json({detail:err, data:[]})
-    }   
+      res.status(400).json({ detail: err, data: [] });
+    }
   }
   static async getGamesByDate(req, res, next) {
     try {
@@ -198,14 +203,63 @@ group by 1, 2, 3`,
     }
   }
   static async createGame(req, res, next) {
+    console.log(req.body);
     try {
-      const newGame = await db.query(
-        "INSERT INTO game_recap.Games (game_name, release_date, synopsis) values ($1, $2, $3) returning *",
-        [req.body.game_name, req.body.release_date, req.body.synopsis]
+      const rows = await db.query(
+        "INSERT INTO game_recap.Games (game_name, release_date, synopsis, merchandise_link) values ($1, $2, $3, $4) returning *",
+        [
+          req.body.game_name,
+          req.body.release_date,
+          req.body.synopsis,
+          req.body.merch_link,
+        ]
       );
-      res.status(200).json({
-        data: newGame.rows[0],
-      });
+      const newGame = rows.rows[0];
+      for (let i = 0; i < req.body.category.length; i++) {
+        const rows = await db.query(
+          "INSERT INTO game_recap.GamesCategories (game_id, c_id) values ($1, $2) returning *",
+          [newGame.game_id, parseInt(req.body.category[i])]
+        );
+      }
+      for (let i = 0; i < req.body.platform.length; i++) {
+        const rows = await db.query(
+          "INSERT INTO game_recap.GamesPlatforms (game_id, platform_id) values ($1, $2) returning *",
+          [newGame.game_id, parseInt(req.body.platform[i])]
+        );
+        console.log(i);
+      }
+      for (let i = 0; i < req.body.devs.length; i++) {
+        const rows = await db.query(
+          "INSERT INTO game_recap.GamesDevelopment (game_id, d_studio_id) values ($1, $2) returning *",
+          [newGame.game_id, parseInt(req.body.devs[i])]
+        );
+      }
+      for (let i = 0; i < req.body.pubs.length; i++) {
+        const rows = await db.query(
+          "INSERT INTO game_recap.GamesPublishing (game_id, p_studio_id) values ($1, $2) returning *",
+          [newGame.game_id, parseInt(req.body.pubs[i])]
+        );
+      }
+      for (let i = 0; i < req.body.subs.length; i++) {
+        const rows = await db.query(
+          "INSERT INTO game_recap.GamesSubscriptionServices (game_id, service_id) values ($1, $2) returning *",
+          [newGame.game_id, parseInt(req.body.subs[i])]
+        );
+      }
+      for (let i = 0; i < req.body.lang.length; i++) {
+        const rows = await db.query(
+          "INSERT INTO game_recap.LanguagesAvailable (game_id, lang_id) values ($1, $2) returning *",
+          [newGame.game_id, parseInt(req.body.lang[i])]
+        );
+      }
+      for (let i = 0; i < req.body.store.length; i++) {
+        const rows = await db.query(
+          "INSERT INTO game_recap.GamesStores (game_id, store_id) values ($1, $2) returning *",
+          [newGame.game_id, parseInt(req.body.store[i])]
+        );
+      }
+      console.log("Çaktıks");
+      res.status(201).json({ message: "Success amk!" });
     } catch (error) {
       console.log(`Error when creating game ${error}`);
       res.status(400).json({ error: error, data: [] });
@@ -238,9 +292,6 @@ group by 1, 2, 3`,
     }
   }
 
-
-
-  
   static async deleteById(req, res, next) {
     try {
       const results = await db.query(
